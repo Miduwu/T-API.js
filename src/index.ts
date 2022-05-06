95ccfe0d3d57a45daaabf68159ab9775a1aa0f70
Mid <83442808+Miduwu@users.noreply.github.com>
2022-05-06 17:05:33 -0500

    Bumped version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import fetch, { Response } from 'node-fetch'
+import axios, { AxiosResponse } from 'axios'
 
 const HOST: string = 'https://api.miduwu.ga/'
 
@@ -10,36 +10,36 @@ function parseParams(obj?: Record<string, any>): string {
 
 export async function Image(endpoint: string, parameters: Record<string, any>): Promise<any> {
     const path: string = `${HOST}image/${endpoint}${parseParams(parameters)}`
-    const res: Response = await fetch(path).catch()
+    const res: AxiosResponse = await axios.get(path, { responseType: 'arraybuffer' }).catch(e => e.response)
     if(res.status === 404) throw new TypeError('The endpoint provided was not found.')
     if(res.status === 400) throw new TypeError('Parameter options provided are invalid.')
     if(res.status === 500) throw new Error('Something internal went wrong with the server, try again later.')
-    return (await res.buffer())
+    return (await res.data)
 }
 
 export async function Json(endpoint: string, parameters?: Record<string, any>): Promise<any> {
     const path: string = `${HOST}json/${endpoint}${parseParams(parameters)}`
-    const res: Response = await fetch(path).catch()
+    const res: AxiosResponse = await axios.get(path).catch(e => e.response)
     if(res.status === 404) throw new TypeError('The endpoint provided was not found.')
     if(res.status === 400) throw new TypeError('Parameter options provided are invalid.')
     if(res.status === 500) throw new Error('Something internal went wrong with the server, try again later.')
-    return (await res.json())
+    return res.data
 }
 
 export async function Anime(endpoint: string, parameters?: Record<string, any>): Promise<any> {
     const path: string = `${HOST}anime/${endpoint}${parseParams(parameters)}`
-    const res: Response = await fetch(path).catch()
+    const res: AxiosResponse = await axios.get(path).catch(e => e.response)
     if(res.status === 404) throw new TypeError('The endpoint provided was not found.')
     if(res.status === 400) throw new TypeError('Parameter options provided are invalid.')
     if(res.status === 500) throw new Error('Something internal went wrong with the server, try again later.')
-    return (await res.json())
+    return res.data
 }
 
 export async function getInfo(endpoint: string): Promise<any> {
     const path: string = `${HOST}endpoint?name=${endpoint}`
-    const res: Response = await fetch(path).catch()
+    const res: AxiosResponse = await axios.get(path).catch(e => e.response)
     if(res.status === 404) throw new TypeError('The endpoint provided was not found.')
     if(res.status === 400) throw new TypeError('Parameter options provided are invalid.')
     if(res.status === 500) throw new Error('Something internal went wrong with the server, try again later.')
-    return (await res.json())
+    return res.data
 }
