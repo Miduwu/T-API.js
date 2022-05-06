5193687b70fb9c73f6f7bbccb0c88635962f5ff8
Mid <83442808+Miduwu@users.noreply.github.com>
2022-05-06 13:06:56 -0500

    Added .d.ts

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export async function Image(endpoint: string, parameters?: Record<string, any>): Promise<any>;
+export async function Json(endpoint: string, parameters?: Record<string, any>): Promise<any>;
+export async function Anime(endpoint: string, parameters?: Record<string, any>): Promise<any>;
+export async function getInfo(endpoint: string): Promise<any>;
--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ export async function Image(endpoint: string, parameters: Record<string, any>): 
     return (await res.buffer())
 }
 
-export async function Json(endpoint: string, parameters: Record<string, any>): Promise<any> {
+export async function Json(endpoint: string, parameters?: Record<string, any>): Promise<any> {
     const path: string = `${HOST}json/${endpoint}${parseParams(parameters)}`
     const res: Response = await fetch(path).catch()
     if(res.status === 404) throw new TypeError('The endpoint provided was not found.')
@@ -26,7 +26,7 @@ export async function Json(endpoint: string, parameters: Record<string, any>): P
     return (await res.json())
 }
 
-export async function Anime(endpoint: string, parameters: Record<string, any>): Promise<any> {
+export async function Anime(endpoint: string, parameters?: Record<string, any>): Promise<any> {
     const path: string = `${HOST}anime/${endpoint}${parseParams(parameters)}`
     const res: Response = await fetch(path).catch()
     if(res.status === 404) throw new TypeError('The endpoint provided was not found.')
